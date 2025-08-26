3b79d9d14d90cb40598e704515c7fadf9b0a6ebe
prossm <patrick.ross.metzger@gmail.com>
2025-08-25 22:23:50 -0400

    update package name to chirpkit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='insect-sound-classifier',
+    name='chirpkit',
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
