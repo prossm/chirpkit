14fe0c2ccd4c432d613b11cd69e781bf408c3e5d
prossm <patrick.ross.metzger@gmail.com>
2025-10-13 14:23:32 -0400

    Add missing __init__.py to transfer_learning package

---
+++ src/transfer_learning/__init__.py
@@ -0,0 +1,9 @@
+"""
+Transfer learning utilities for ChirpKit
+
+Provides BirdNET embedding extraction for insect sound classification.
+"""
+
+from .birdnet_embeddings import BirdNETEmbeddingExtractor
+
+__all__ = ['BirdNETEmbeddingExtractor']
